6b0d8a968f5523d5a8e3b31f1bec48e8ad22756e
David Kielty <davidkielty@live.co.uk>
2023-11-28 17:09:48 +0000

    Next14 Houseclean

--- src/env.js
+++ src/env.js
@@ -38,19 +38,19 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_DATABASE_URL: z.string().url(),
-    NEXT_PUBLIC_NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
-    ),
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: z.string(),
-    NEXT_PUBLIC_GITHUB_CLIENT_SECRET: z.string(),
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: z.string(),
+    // NEXT_PUBLIC_NEXTAUTH_URL: z.preprocess(
+    //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    //   (str) => process.env.VERCEL_URL ?? str,
+    //   // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    //   process.env.VERCEL ? z.string() : z.string().url(),
+    // ),
+    // NEXT_PUBLIC_GITHUB_CLIENT_ID: z.string(),
+    // NEXT_PUBLIC_GITHUB_CLIENT_SECRET: z.string(),
+    // NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
+    // NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: z.string(),
     NEXT_PUBLIC_MAPTOKEN: z.string(),
-    NEXT_PUBLIC_MAPBOX_URL: z.string().url(),
+    // NEXT_PUBLIC_MAPBOX_URL: z.string().url(),
   },
 
   /**
@@ -64,24 +64,22 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-
     MAPTOKEN: process.env.MAPTOKEN,
     MAPBOX_URL: process.env.MAPBOX_URL,
 
     /* Next Public*/
     // NEXT_PUBLIC_DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    // NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    // NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    NEXT_PUBLIC_GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    // NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    // NEXT_PUBLIC_GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 
     NEXT_PUBLIC_MAPTOKEN: process.env.MAPTOKEN,
-    NEXT_PUBLIC_MAPBOX_URL: process.env.MAPBOX_URL,
+    // NEXT_PUBLIC_MAPBOX_URL: process.env.MAPBOX_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
