756fdc2abc2d5a19a48136d3808d5538d8603fa5
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2023-12-04 00:27:34 +0000

    Lint Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { type NextPage } from "next";
 import Head from "next/head";
 
 import HomeModal from "~/components/HomeModal";
-
-// require("dotenv").config();
 
 const Home: NextPage = () => {
   return (
