a4da4c5b74ac3e82ce46cd70c61542d48965c559
DavidKielty1 <78271797+DavidKielty1@users.noreply.github.com>
2023-12-04 00:30:59 +0000

    Lint Cleanup

--- src/env.js
+++ src/env.js
@@ -46,15 +46,17 @@ export const env = createEnv({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     //   process.env.VERCEL ? z.string() : z.string().url(),
     // ),
-    NEXT_PUBLIC_DATABASE_URL: z.string().url(),
-    NEXT_PUBLIC_NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: z.string(),
-    NEXT_PUBLIC_GITHUB_CLIENT_SECRET: z.string(),
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: z.string(),
+
+    // NEXT_PUBLIC_DATABASE_URL: z.string().url(),
+    // NEXT_PUBLIC_NEXTAUTH_SECRET:
+    //   process.env.NODE_ENV === "production"
+    //     ? z.string()
+    //     : z.string().optional(),
+    // NEXT_PUBLIC_GITHUB_CLIENT_ID: z.string(),
+    // NEXT_PUBLIC_GITHUB_CLIENT_SECRET: z.string(),
+    // NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
+    // NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: z.string(),
+
     NEXT_PUBLIC_MAPTOKEN: z.string(),
   },
 
@@ -77,14 +79,16 @@ export const env = createEnv({
 
     // NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
     // NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-    NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
-    NEXT_PUBLIC_NEXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET:
-      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-    NEXT_PUBLIC_GITHUB_CLIENT_SECRET:
-      process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+
+    // NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    // NEXT_PUBLIC_NEXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    // NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    // NEXT_PUBLIC_GOOGLE_CLIENT_SECRET:
+    //   process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    // NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    // NEXT_PUBLIC_GITHUB_CLIENT_SECRET:
+    //   process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+
     NEXT_PUBLIC_MAPTOKEN: process.env.NEXT_PUBLIC_MAPTOKEN,
   },
   /**
